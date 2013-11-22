749c923174ebc48b0dba7bc00f22dc8ecfe9a6a8
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-22 09:27:24 -0800

    Remove internal validation loading

--- lib/fission-validator.rb
+++ lib/fission-validator.rb
@@ -1,7 +1,3 @@
 require 'fission'
 require 'fission-validator/version'
 require 'fission-validator/validator'
-
-Dir.glob(File.join(File.dirname(__FILE__), 'fission-validator', 'validations', '*.rb')).each do |path|
-  require "fission-validator/#{File.basename(path).sub('.rb', '')}"
-end
