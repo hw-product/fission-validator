c841ccab3cda4bcc627adc5bb3dfb7e2baa736e7
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-30 07:26:06 -0800

    Add spec for bad payload

--- test/specs/github.rb
+++ test/specs/github.rb
@@ -13,10 +13,20 @@ describe 'Fission::Validator::Github' do
     @runner.terminate
   end
 
-  it 'should add user to payload' do
+  it 'should add account to payload' do
     Celluloid::Actor[:validator].transmit(payload_for(:github, :nest => :github))
     source_wait
     result = MessageStore.messages.pop
+    result.wont_be_nil
     Carnivore::Utils.retrieve(result, :data, :account).wont_be_nil
   end
+
+  it 'should set error state if payload is invalid' do
+    Celluloid::Actor[:validator].transmit(Fission::Utils.new_payload(:fubar, {:github => {:bad => :state}}))
+    source_wait
+    result = MessageStore.messages.pop
+    result.wont_be_nil
+    Carnivore::Utils.retrieve(result, :data, :account).must_be_nil
+  end
+
 end
