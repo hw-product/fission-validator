c13d6615e2f5a40d7c5652e71d905a512509fa6c
Chris Roberts <chrisroberts.code@gmail.com>
2014-04-14 22:16:52 -0700

    Use generic repository format

--- lib/fission-validator/github.rb
+++ lib/fission-validator/github.rb
@@ -25,52 +25,54 @@ module Fission
       end
 
       def execute(message)
-        payload = unpack(message)
-        git_uri = retrieve(payload, :data, :github, :repository, :url)
-        if(git_uri)
-          repository = Fission::Data::Repository.find_by_matching_url(git_uri)
-          unless(repository)
-            account_name = retrieve(payload, :data, :github, :repository, :owner, :name)
-            account = Fission::Data::Account.lookup(account_name, :github, :remote)
-            if(account && account.active?)
-              if(account.new?)
-                warn "Discovered previously existing account not in data store. Adding (#{account.inspect})"
-                account.save
+        failure_wrap(message) do |payload|
+          format_payload(payload, :repository, :github)
+          git_uri = payload.get(:data, :format, :repository, :url)
+          if(git_uri)
+            repository = Fission::Data::Repository.find_by_matching_url(git_uri)
+            unless(repository)
+              account_name = payload.get(:data, :format, :repository, :owner_name)
+              account = Fission::Data::Account.lookup(account_name, :github, :remote)
+              if(account && account.active?)
+                if(account.new?)
+                  warn "Discovered previously existing account not in data store. Adding (#{account.inspect})"
+                  account.save
+                end
+                info "Unregistered repository encountered for active account: #{account}. Adding."
+                repository = Fission::Data::Repository.new(
+                  :name => [
+                    payload.get(:data, :format, :repository, :owner_name),
+                    payload.get(:data, :format, :repository, :name)
+                  ].join('/'),
+                  :source => :github,
+                  :private => payload.get(:data, :format, :repository, :private),
+                  :url => git_uri,
+                  :clone_url => git_uri.sub('git:', 'https:')
+                )
+                repository.owner = account
+                repository.save
               end
-              info "Unregistered repository encountered for active account: #{account}. Adding."
-              repository = Fission::Data::Repository.new(
-                :name => [
-                  retrieve(payload, :data, :github, :repository, :owner, :name),
-                  retrieve(payload, :data, :github, :repository, :name)
-                ].join('/'),
-                :source => :github,
-                :private => retrieve(payload, :data, :github, :repository, :private),
-                :url => retrieve(payload, :data, :github, :repository, :url),
-                :clone_url => retrieve(payload, :data, :github, :repository, :url).sub('git:', 'https:')
-              )
-              repository.owner = account
-              repository.save
             end
-          end
-          if(repository)
-            debug "Account found for #{message}: #{repository.owner.id}"
-            payload[:data][:account] = {
-              :id => repository.owner.id,
-              :name => repository.owner.name
-            }
-            debug 'Saving job into data store'
-            job = Fission::Data::Job.new(
-              :message_id => payload[:message_id],
-              :payload => payload
-            )
-            job.account = repository.owner
-            job.save
-            completed(payload, message)
+            if(repository)
+              debug "Account found for #{message}: #{repository.owner.id}"
+              payload[:data][:account] = {
+                :id => repository.owner.id,
+                :name => repository.owner.name
+              }
+              debug 'Saving job into data store'
+              job = Fission::Data::Job.new(
+                :message_id => payload[:message_id],
+                :payload => payload
+              )
+              job.account = repository.owner
+              job.save
+              completed(payload, message)
+            else
+              failed(payload, message, 'Failed to locate registered repository using given location')
+            end
           else
-            failed(payload, message, 'Failed to locate registered repository using given location')
+            failed(payload, message, 'No repository location found in payload')
           end
-        else
-          failed(payload, message, 'No repository location found in payload')
         end
       end
 
