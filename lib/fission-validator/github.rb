eb945be5df5d27187a87b9e413ff5bff66ae721b
Chris Roberts <chrisroberts.code@gmail.com>
2014-11-29 07:43:29 -0800

    Remove automatic repository enable feature (no longer valid with custom api user)

--- lib/fission-validator/github.rb
+++ lib/fission-validator/github.rb
@@ -34,29 +34,6 @@ module Fission
           git_uri = payload.get(:data, :format, :repository, :url)
           if(git_uri)
             repository = Fission::Data::Models::Repository.find_by_matching_url(git_uri)
-            unless(repository)
-              account_name = payload.get(:data, :format, :repository, :owner_name)
-              account = Fission::Data::Account.lookup(account_name, :github, :remote)
-              if(account && account.active?)
-                if(account.new?)
-                  warn "Discovered previously existing account not in data store. Adding (#{account.inspect})"
-                  account.save
-                end
-                info "Unregistered repository encountered for active account: #{account}. Adding."
-                repository = Fission::Data::Repository.new(
-                  :name => [
-                    payload.get(:data, :format, :repository, :owner_name),
-                    payload.get(:data, :format, :repository, :name)
-                  ].join('/'),
-                  :source => :github,
-                  :private => payload.get(:data, :format, :repository, :private),
-                  :url => git_uri,
-                  :clone_url => git_uri.sub('git:', 'https:')
-                )
-                repository.owner = account
-                repository.save
-              end
-            end
             if(repository)
               debug "Account found for #{message}: #{repository.owner.id}"
               payload[:data][:account] = {
