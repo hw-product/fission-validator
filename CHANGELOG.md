afdf844b3fedc4126c8ede73c4014385b76f1b8e
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-04 11:30:32 -0800

    Update files for release v0.1.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
-## v0.1.0
+# v0.1.4
+* Refactor github validator to use formatters and be generic
+
+# v0.1.0
 * Initial release
--- lib/fission-validator/version.rb
+++ lib/fission-validator/version.rb
@@ -1,5 +1,5 @@
 module Fission
   module Validator
-    VERSION = Gem::Version.new('0.1.3')
+    VERSION = Gem::Version.new('0.1.4')
   end
 end
