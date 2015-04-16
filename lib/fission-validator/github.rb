b5785d8da7d890055a5eb8c9645fadff0d42d708
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-16 06:51:24 -0700

    If account has custom configuration items defined, include within payload

--- lib/fission-validator/github.rb
+++ lib/fission-validator/github.rb
@@ -41,13 +41,25 @@ module Fission
           )
           if(repository)
             account = repository.account
-            info "Message validated with account #{account} (#{message})"
-            payload.set(:data, :account,
-              Smash.new(
-                :id => account.id,
-                :name => account.name
+            if(account.account_configs && !account.account_configs.empty?)
+              account_config = Smash.new.tap do |a_config|
+                account.account_configs.each do |ac|
+                  a_config.deep_merge!(ac)
+                end
+              end
+              account_config = Fission::Utils::Cipher.encrypt(
+                MultiJson.dump(account_config),
+                :iv => payload[:message_id],
+                :key => app_config.fetch(:grouping, DEFAULT_SECRET)
               )
+            end
+            account_info = Smash.new(
+              :id => account.id,
+              :name => account.name
             )
+            account_info[:config] = account_config if account_config
+            info "Message validated with account #{account} (#{message})"
+            payload.set(:data, :account, account_info)
             job_completed(:validator, payload, message)
           else
             error "Failed to validate message. Repository is not registered! (#{message})"
